210142da2a4994c72e077edebd19d3ec5dda72ff
Aaron Shaw <shawaj@gmail.com>
2021-09-15 02:08:28 +0100

    Update test_parse_config.py

--- miner_config/tests/test_parse_config.py
+++ miner_config/tests/test_parse_config.py
@@ -44,7 +44,7 @@ class TestExtractBlessedBlock(TestCase):
         content = load_fixture('sample_upstream_config.txt')
         block = extract_blessed_block(content)
         self.assertIsInstance(block, int)
-        self.assertEqual(block, 910531)
+        self.assertEqual(block, 1009441)
 
     def test_block_invalid_type(self):
         # Expect an invalid type for blessed block id to through
@@ -86,8 +86,8 @@ class TestExtractBlessedBlockHash(TestCase):
         hash = extract_blessed_block_hash(content)
         self.assertIsInstance(hash, str)
         expected = (
-            '<<95,253,152,33,253,132,175,201,16,213,118,213,1,87,113,203,209'
-            ',242,143,230,104,213,232,83,61,173,201,176,206,176,178,42>>'
+            '<<37,94,93,250,210,94,95,89,182,55,53,240,112,56,24,102,202,225,'
+            '178,147,150,142,41,253,7,233,51,6,232,192,174,250>>'
         )
         self.assertEqual(hash, expected)
 
@@ -131,11 +131,11 @@ class TestCaseGetBlessedBlock(TestCase):
         content = load_fixture('sample_upstream_config.txt')
         result = get_blessed_block(content)
         hash = (
-            '<<95,253,152,33,253,132,175,201,16,213,118,213,1,87,113,203,'
-            '209,242,143,230,104,213,232,83,61,173,201,176,206,176,178,42>>'
+            '<<37,94,93,250,210,94,95,89,182,55,53,240,112,56,24,102,202,225,'
+            '178,147,150,142,41,253,7,233,51,6,232,192,174,250>>'
         )
         expected = {
-            'blessed_block': 910531,
+            'blessed_block': 1009441,
             'blessed_block_hash': hash}
         self.assertEqual(result, expected)
         self.assertIsInstance(result, dict)
