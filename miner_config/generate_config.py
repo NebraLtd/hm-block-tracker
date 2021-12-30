89e4ec5ff7f7ee7e572b1a43659b1d55e2fd6a95
Ilya Starodubtsev <ilyastarodubtsev55@gmail.com>
2021-12-30 20:53:17 +0600

    Update: Implement Sentry

--- miner_config/generate_config.py
+++ miner_config/generate_config.py
@@ -1,6 +1,15 @@
 import os
 import requests
 from jinja2 import Template
+import sentry_sdk
+
+
+def init_sentry():
+    dsn_sentry = os.environ.get("DSN_SENTRY")
+    sentry_sdk.init(
+        dsn_sentry,
+        traces_sample_rate=1.0,
+    )
 
 
 def get_latest_snapshot_block(base_url):
@@ -46,6 +55,7 @@ def output_config_file(config, path):
 
 
 def main():
+    init_sentry()
     if bool(int(os.getenv('PRODUCTION', '0'))):
         base_url = 'https://helium-snapshots.nebra.com'
     else:
