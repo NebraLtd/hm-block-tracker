ae59a3ead1c935e7f2836adfe100740beffb3c4f
Viktor Petersson <vpetersson@users.noreply.github.com>
2022-02-08 17:50:18 +0000

    Refs #44. Adds initial sanity check and strict bash mode (#64)
    * Refs #44. Adds initial sanity check and strict bash mode
    
    * fix: only upload snapshots once
    
    Co-authored-by: Marvin Arnold <marvin@geeky.rocks>

--- snapshotter/create_snapshot.sh
+++ snapshotter/create_snapshot.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
 
 export PATH=$PATH:/usr/bin:/snap/bin
 
@@ -15,13 +17,26 @@ BLOCK_HASH_PART2=$(docker exec miner miner snapshot info /var/data/saved-snaps/l
 BYTE_ARRAY="${BLOCK_HASH_PART1}${BLOCK_HASH_PART2}"
 BASE64URL_FORMAT=$(python3 /home/snapshot/hm-block-tracker/snapshotter/base64url_encoder.py "$BYTE_ARRAY")
 
-mv /var/miner_data/saved-snaps/latest /var/miner_data/saved-snaps/snap-$BLOCK_HEIGHT
+mv /var/miner_data/saved-snaps/latest "/var/miner_data/saved-snaps/snap-$BLOCK_HEIGHT"
 echo "{\"height\": $BLOCK_HEIGHT, \"hash\": \"$BYTE_ARRAY\"}" > /var/miner_data/saved-snaps/latest.json
 echo "{\"height\": $BLOCK_HEIGHT, \"hash\": \"$BASE64URL_FORMAT\"}" > /var/miner_data/saved-snaps/latest-snap.json
 
-gsutil cp /var/miner_data/saved-snaps/snap-$BLOCK_HEIGHT "gs://$SNAPSHOT_BUCKET/snap-$BLOCK_HEIGHT"
-gsutil cp /var/miner_data/saved-snaps/latest.json "gs://$SNAPSHOT_BUCKET/latest.json"
-gsutil cp /var/miner_data/saved-snaps/latest-snap.json "gs://$SNAPSHOT_BUCKET/latest-snap.json"
+# Ensure neither snaphot 'height' nor 'hash' are empty.
+are_snapshots_valid=1
+for file in /var/miner_data/saved-snaps/{latest.json,latest-snap.json}; do
+    sanity_check=$(jq '.[] | select(. == null or . == "")' < "$file")
+    if [ -n "$sanity_check" ]; then
+        echo "'hash' or 'height' returned empty in $file. Will not upload snapshots."
+        are_snapshots_valid=0
+    fi
+done
+
+# Only upload snapshots if they are all valid.
+if [ "$are_snapshots_valid" -eq "1" ]
+    gsutil cp "/var/miner_data/saved-snaps/snap-$BLOCK_HEIGHT" "gs://$SNAPSHOT_BUCKET/snap-$BLOCK_HEIGHT"
+    gsutil cp /var/miner_data/saved-snaps/latest.json "gs://$SNAPSHOT_BUCKET/latest.json"
+    gsutil cp /var/miner_data/saved-snaps/latest-snap.json "gs://$SNAPSHOT_BUCKET/latest-snap.json"
+fi
 
 rm docker.config
-rm -f /var/miner_data/saved-snaps/snap-$BLOCK_HEIGHT
+rm -f "/var/miner_data/saved-snaps/snap-$BLOCK_HEIGHT"
