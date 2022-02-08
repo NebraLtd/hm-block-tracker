79905eb637463d6482660cca5de6009aac02544f
Viktor Petersson <viktor@nebra.com>
2022-02-08 18:35:10 +0000

    More hotfixes to bring blocktracker up to date

--- snapshotter/create_snapshot.sh
+++ snapshotter/create_snapshot.sh
@@ -16,16 +16,15 @@ BLOCK_HASH_PART1=$(docker exec miner miner snapshot info /var/data/saved-snaps/l
 BLOCK_HASH_PART2=$(docker exec miner miner snapshot info /var/data/saved-snaps/latest | tail -2 | head -1 | awk {'print $1'})
 BYTE_ARRAY="${BLOCK_HASH_PART1}${BLOCK_HASH_PART2}"
 BASE64URL_FORMAT=$(python3 /home/snapshot/hm-block-tracker/snapshotter/base64url_encoder.py "$BYTE_ARRAY")
-LATEST_JSON="/home/snapshot/latest.json"
-LATEST_SNAP_JSON="/home/snapshot/latest-snap.json"
+TMPDIR=$(mktemp -d)
 
-mv /var/miner_data/saved-snaps/latest "/var/miner_data/saved-snaps/snap-$BLOCK_HEIGHT"
-echo "{\"height\": $BLOCK_HEIGHT, \"hash\": \"$BYTE_ARRAY\"}" > "$LATEST_JSON"
-echo "{\"height\": $BLOCK_HEIGHT, \"hash\": \"$BASE64URL_FORMAT\"}" > "$LATEST_SNAP_JSON"
+mv /var/miner_data/saved-snaps/latest "$TMPDIR/snap-$BLOCK_HEIGHT"
+echo "{\"height\": $BLOCK_HEIGHT, \"hash\": \"$BYTE_ARRAY\"}" | tee -a "$TMPDIR/latest.json"
+echo "{\"height\": $BLOCK_HEIGHT, \"hash\": \"$BASE64URL_FORMAT\"}" | tee -a "$TMPDIR/latest-snap.json"
 
 # Ensure neither snaphot 'height' nor 'hash' are empty.
 are_snapshots_valid=1
-for file in /home/snapshot/{latest.json,latest-snap.json}; do
+for file in $TMPDIR/{latest.json,latest-snap.json}; do
     sanity_check=$(jq '.[] | select(. == null or . == "")' < "$file")
     if [ -n "$sanity_check" ]; then
         echo "'hash' or 'height' returned empty in $file. Will not upload snapshots."
@@ -35,10 +34,9 @@ done
 
 # Only upload snapshots if they are all valid.
 if [ "$are_snapshots_valid" -eq "1" ]; then
-    gsutil cp "/var/miner_data/saved-snaps/snap-$BLOCK_HEIGHT" "gs://$SNAPSHOT_BUCKET/snap-$BLOCK_HEIGHT"
-    gsutil cp "$LATEST_JSON" "gs://$SNAPSHOT_BUCKET/latest.json"
-    gsutil cp "$LATEST_SNAP_JSON" "gs://$SNAPSHOT_BUCKET/latest-snap.json"
+    gsutil cp "$TMPDIR/snap-$BLOCK_HEIGHT" "gs://$SNAPSHOT_BUCKET/snap-$BLOCK_HEIGHT"
+    gsutil cp "$TMPDIR/latest.json" "gs://$SNAPSHOT_BUCKET/latest.json"
+    gsutil cp "$TMPDIR/latest-snap.json" "gs://$SNAPSHOT_BUCKET/latest-snap.json"
 fi
 
-rm docker.config
-rm -f "/var/miner_data/saved-snaps/snap-$BLOCK_HEIGHT"
+rm -rf "$TMPDIR" docker.config
