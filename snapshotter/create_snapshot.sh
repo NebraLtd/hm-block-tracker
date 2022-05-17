b15af8807acfccc268be9cc8037237fcfd83fde1
Marvin Arnold <marvin@geeky.rocks>
2022-05-16 19:32:04 -0500

    logs: create_snapshot.sh progress

--- snapshotter/create_snapshot.sh
+++ snapshotter/create_snapshot.sh
@@ -27,9 +27,16 @@ fi
 
 
 # Initial sanity checks passed. Continuing on with a snapshot.
+echo "About to take snapshot"
 docker exec miner miner snapshot take /var/data/saved-snaps/latest
+
+echo "Parsing snapshot (1/3)"
 BLOCK_HEIGHT=$(docker exec miner miner snapshot info /var/data/saved-snaps/latest | head -1 | awk {'print $2'})
+
+echo "Parsing snapshot (2/3)"
 BLOCK_HASH_PART1=$(docker exec miner miner snapshot info /var/data/saved-snaps/latest | tail -3 | head -1 | awk {'print $2'})
+
+echo "Parsing snapshot (3/3)"
 BLOCK_HASH_PART2=$(docker exec miner miner snapshot info /var/data/saved-snaps/latest | tail -2 | head -1 | awk {'print $1'})
 BYTE_ARRAY="${BLOCK_HASH_PART1}${BLOCK_HASH_PART2}"
 BASE64URL_FORMAT=$(python3 /home/snapshot/hm-block-tracker/snapshotter/base64url_encoder.py "$BYTE_ARRAY")
@@ -40,6 +47,7 @@ echo "{\"height\": $BLOCK_HEIGHT, \"hash\": \"$BYTE_ARRAY\"}" | tee -a "$TMPDIR/
 echo "{\"height\": $BLOCK_HEIGHT, \"hash\": \"$BASE64URL_FORMAT\"}" | tee -a "$TMPDIR/latest-snap.json"
 
 # Ensure neither snaphot 'height' nor 'hash' are empty.
+echo "Verifying snapshot integrity"
 ARE_SNAPSHOTS_VALID=1
 for file in $TMPDIR/{latest.json,latest-snap.json}; do
     SANITY_CHECK=$(jq '.[] | select(. == null or . == "")' < "$file")
@@ -51,9 +59,11 @@ done
 
 # Only upload snapshots if they are all valid.
 if [ "$ARE_SNAPSHOTS_VALID" -eq "1" ]; then
+    echo "Copying snapshot to GCS"
     gsutil cp "$TMPDIR/snap-$BLOCK_HEIGHT" "gs://$SNAPSHOT_BUCKET/snap-$BLOCK_HEIGHT.gz"
     gsutil cp "$TMPDIR/latest.json" "gs://$SNAPSHOT_BUCKET/latest.json"
     gsutil cp "$TMPDIR/latest-snap.json" "gs://$SNAPSHOT_BUCKET/latest-snap.json"
 fi
 
+echo "Removing TMPDIR and docker.config. Finished."
 rm -rf "$TMPDIR" docker.config
